92328b1de6a9c64d8152ed04883f4003a5a4bc98
Ulidtsoa01 <22529166+Ulidtsoa01@users.noreply.github.com>
2024-09-18 00:11:35 -0400

    step4

--- server/index.js
+++ server/index.js
@@ -103,7 +103,7 @@ app.get('/rest/old/importquotejson', backup.importQuoteJsonOld);
 // });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname, 'frontend/build'));
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
